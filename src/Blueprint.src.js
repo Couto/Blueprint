923b26ac57736c51ec4333fcf71f552092e594f2
Couto <lcouto87@gmail.com>
2012-01-20 03:43:27 +0000

    JSLint, clear whitespaces, reformat to be more readable

--- src/Blueprint.src.js
+++ src/Blueprint.src.js
@@ -7,94 +7,100 @@
  * @version 0.1
  *
  * @license
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+ *     This program is free software. It comes without any warranty, to
+ *     the extent permitted by applicable law. You can redistribute it
+ *     and/or modify it under the terms of the Do What The Fuck You Want
+ *     To Public License, Version 2, as published by Sam Hocevar. See
+ *     http://sam.zoy.org/wtfpl/COPYING for more details.
  *
  * @copyright 2012, Luis Couto
  *
- * @param {Object} methods Object with methods
+ * @param {Object} methods Object
  * @returns Function
  */
-function Blueprint (methods) {
+function Blueprint(methods) {
+    'use strict';
+
     var blueprint;
-    
-    
+
+
+
     /**
      * Extends an object with another given object
      *
      * @private
-     
+     *
      * @param {Object} target Object's that will get the new methods
      * @returns undefined
      */
-    function extend (methods, target) {
+    function extend(methods, target) {
         var k;
         for (k in methods) {
-            if (methods.hasOwnProperty(k)){ target[k] = methods[k]; }
+            if (methods.hasOwnProperty(k)) {
+                target[k] = methods[k];
+            }
         }
     }
-    
-    
+
+
+
     /**
-     * For an Array of Objects, add their methods/properties to 
+     * For an Array of Objects, add their methods/properties to
      * target's prototype
      *
      * @private
      * @param {Array} arr Array of objects that will give their methods
      * @param {Object} Target that will receive the methods
      * @returns undefined
-     */ 
-    function implement (arr, target){
+     */
+    function implement(arr, target) {
 
         var i = arr.length - 1,
-            k, constructorBck;
-            
+            constructorBck;
+
         for (i; i >= 0; i -= 1) {
             if (arr[i].prototype.constructor) {
                 constructorBck = arr[i].prototype.constructor;
                 delete arr[i].prototype.constructor;
                 extend(arr[i].prototype, target.prototype);
                 arr[i].prototype.constructor = constructorBck;
-            } else { 
-                extend(arr[i].prototype, target.prototype); 
+            } else {
+                extend(arr[i].prototype, target.prototype);
             }
         }
     }
-    
-    
-    
+
+
+
     /**
      * Fixes the context in given methods
      *
      * @private
-     * @param {Function} 
+     * @param {Function}
      * @returns function handler with fixed context
      */
-    function proxies (arr, context, target) {
-        var proxy = function(func){
-            
-            if (Function.prototype.bind) { 
-                return func.bind(context); 
-            } 
+    function proxies(arr, context, target) {
+        var proxy = function (func) {
+
+            if (Function.prototype.bind) {
+                return func.bind(context);
+            }
 
             return function () {
                 return func.apply(context, arguments);
             };
 
         }, i = arr.length - 1;
-        
-        for (i; i >= 0; i -=1 ) {
+
+        for (i; i >= 0; i -= 1) {
             target[arr[i]] = proxy(target[arr[i]], blueprint);
         }
     }
-    
-    
-    
+
+
+
     /**
-     * Copies the given object into a freshly 
+     * Copies the given object into a freshly
      * created empty function's prototype
      *
      * @private
@@ -102,31 +108,35 @@ function Blueprint (methods) {
      * @returns {Function} Instance
      * @type Function
      */
-    function clone (o) {
-        function F(){}
+    function clone(o) {
+        function F() {}
         F.prototype = o;
         return new F();
     }
-    
-    
-    
+
+
+
     blueprint = methods.init || function blueprint() {};
-    
+
     if (methods.Extends) {
         blueprint.Super = methods.Extends.prototype;
         blueprint.prototype = clone(blueprint.Super);
         extend(methods, blueprint.prototype);
-    } else { blueprint.prototype = methods; }
+    } else {
+        blueprint.prototype = methods;
+    }
+
     blueprint.prototype.constructor = blueprint;
-    
+
     if (methods.Implements) { implement(methods.Implements, blueprint); }
-    if (methods.Bind)       { proxies(methods.Bind, blueprint, blueprint.prototype); }
-    if (methods.Static)     {
+    if (methods.Bind) { proxies(methods.Bind, blueprint, blueprint.prototype); }
+    if (methods.Static) {
         extend(methods.Static, blueprint);
         delete blueprint.prototype.Static;
     }
-    
-    
-    
+
+
+
     return blueprint;
+
 }
